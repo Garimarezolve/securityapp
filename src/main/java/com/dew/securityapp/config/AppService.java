1cced01d65898f1ec475353e471f8e18e5053755
garimadew <GarimaSharma@resolve.com>
2023-02-28 13:28:02 +0530

    vault changes update  into the project

--- src/main/java/com/dew/securityapp/config/AppService.java
+++ src/main/java/com/dew/securityapp/config/AppService.java
@@ -1,3 +1,4 @@
+/*
 package com.dew.securityapp.config;
 
 import jakarta.annotation.PostConstruct;
@@ -20,3 +21,4 @@ public class AppService {
     }
 
 }
+*/
--- src/main/java/com/dew/securityapp/config/VaultConfig.java
+++ src/main/java/com/dew/securityapp/config/VaultConfig.java
@@ -2,18 +2,15 @@ package com.dew.securityapp.config;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Getter
 @Setter
-@ConfigurationProperties("securityapp")
+@ConfigurationProperties("app.config.auth")
 public class VaultConfig {
 
-    @Value("${spring.datasource.username}")
     private String username;
-    @Value("${spring.datasource.password}")
-    public String password;
+    public String token;
 }
